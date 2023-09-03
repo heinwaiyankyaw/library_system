28387e0b3af425bdb6dbf67c0aca9aaac37c0f72
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-09-03 22:12:30 +0630

    NeedToWatchAgain

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
-function useFetch(url) {
+function useFetch(url, method = "GET") {
   let [data, setData] = useState(null);
+  let [postData, setPostData] = useState(null);
   let [loading, setLoading] = useState(false);
   let [error, setError] = useState(null);
 
@@ -9,31 +10,49 @@ function useFetch(url) {
     let abortController = new AbortController();
     let signal = abortController.signal;
 
-    setLoading(true);
-    fetch(url, {
+    let options = {
       signal,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("something went wrong");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setError(null);
-        setLoading(false);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
+      method,
+    };
+    setLoading(true);
+    let fetchData = () => {
+      fetch(url, options)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("something went wrong");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setError(null);
+          setLoading(false);
+        })
+        .catch((e) => {
+          setError(e.message);
+        });
+    };
+    if (method === "POST" && postData) {
+      options = {
+        ...options,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      };
+      fetchData();
+    }
+
+    if (method === "GET") {
+      fetchData();
+    }
 
     //cleanup function
     return () => {
       abortController.abort();
     };
-  }, [url]);
-  return { data, loading, error };
+  }, [url, postData]);
+  return { setPostData, data, loading, error };
 }
 
 export default useFetch;
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,26 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 export default function Create() {
   let [title, setTitle] = useState("");
   let [description, setDescription] = useState("");
   let [newCategory, setNewCategory] = useState("");
+  let [categories, setCategories] = useState([]);
+
+  let { setPostData, data: book } = useFetch(
+    "http://localhost:3001/books",
+    "POST"
+  );
+  let navigate = useNavigate();
+  let addCategory = (e) => {
+    setCategories((prev) => [newCategory, ...prev]);
+    setNewCategory("");
+  };
+
+  let addBook = (e) => {
+    e.preventDefault();
+    let data = {
+      title,
+      description,
+      categories,
+    };
+    setPostData(data);
+  };
+
+  useEffect(() => {
+    if (book) {
+      navigate("/");
+    }
+  }, [book]);
+
   return (
-    <form className="w-full max-w-lg mx-auto mt-5">
+    <form className="w-full max-w-lg mx-auto mt-5" onSubmit={addBook}>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-password"
+            htmlFor="grid-password"
           >
             Book Title
           </label>
           <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-password"
             type="text"
-            placeholder="Enter Book Title..."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Book Title"
           />
         </div>
       </div>
@@ -28,45 +58,52 @@ export default function Create() {
         <div className="w-full px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-password"
+            htmlFor="grid-password"
           >
             Book Description
           </label>
           <textarea
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-password"
-            type="text"
-            placeholder="Enter Book Description..."
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="password"
+            placeholder="Book Description"
           />
+          <p className="text-gray-600 text-xs italic">
+            Make it as long and as crazy as you'd like
+          </p>
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-password"
+            htmlFor="grid-password"
           >
             Categories
           </label>
           <div className="flex items-center space-x-2">
             <input
+              value={newCategory}
+              onChange={(e) => setNewCategory(e.target.value)}
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-password"
               type="text"
-              placeholder="Enter Book Categories..."
-              value={newCategory}
-              onChange={(e) => setNewCategory(e.target.value)}
+              placeholder="Book Category"
             />
-            <button className="bg-primary mb-3 p-1 rounded-lg">
+            <button
+              type="button"
+              onClick={addCategory}
+              className="bg-primary p-1 rounded-lg mb-3"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6 text-white p-1"
+                className="w-6 h-6 p-1 text-white"
               >
                 <path
                   strokeLinecap="round"
@@ -77,8 +114,35 @@ export default function Create() {
             </button>
           </div>
         </div>
+        <div className="flex flex-wrap">
+          {categories.map((c) => (
+            <span
+              key={c}
+              className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-primary"
+            >
+              {" "}
+              {c}
+            </span>
+          ))}
+        </div>
       </div>
-      <button className="text-white bg-primary px-3 py-2 rounded-2xl flex items-center gap-1 w-full justify-center">
+      {/* create book */}
+      <button className="text-white bg-primary px-3 py-2 rounded-2xl flex justify-center items-center gap-1 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
         <span className="hidden md:block">Create book</span>
       </button>
     </form>
